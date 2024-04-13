af3e449acc05e902a3bd10f502da068c1aa746d2
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2024-04-13 19:35:25 +0700

    Updates

--- src/components/Why_GrabFood.jsx
+++
@@ -1,5 +0,0 @@
-export default function Why_GrabFood(props){
-    return(
-        <li className="list-image-[url(https://food.grab.com/static/images/tick.svg)] mt-3"><span className="font-bold">{props.title} </span> - {props.descriptions}</li>
-    );
-}
